4086af0124066751f683a2f1e1f1b2ca5ddace60
satoylf <luisfelipeyoshiosato@gmail.com>
2024-10-25 15:12:11 -0300

    feat: mongo db connection

---
+++ src/main/java/proj/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package proj.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import proj.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/proj/services/UserService.java
@@ -0,0 +1,20 @@
+package proj.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import proj.domain.User;
+import proj.repository.UserRepository;
+
+@Service
+public class UserService{
+
+  @Autowired
+  private UserRepository repo;
+
+  public List<User> findAll(){
+    return repo.findAll();
+  }
+}
