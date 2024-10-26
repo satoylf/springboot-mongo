ab30af3cb310eab897b2879596918fab493141f6
satoylf <luisfelipeyoshiosato@gmail.com>
2024-10-26 14:08:53 -0300

    feat: custom queries implemented

--- src/main/java/proj/repository/PostRepository.java
+++ src/main/java/proj/repository/PostRepository.java
@@ -1,11 +1,25 @@
 package proj.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import proj.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+  
+  List<Post> findByTitleContainingIgnoreCase(String text); // This method is automatically implemented by Spring Data
+  
+  // ?0 = first parameter, i = case insensitive
+  @Query("{ 'title': { $regex: ?0, $options: 'i' } }") // Custom Query
+  List<Post> searchTitle(String text);
 
+  // ?1 = second parameter, ?2 = third parameter
+  // $gte = greater than or equal, $lte = less than or equal
+  @Query("{ $and: [ { date: {$gte: ?1} }, {date: {$lte: ?2} }, {$or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+  List<Post> completeSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/proj/services/PostService.java
+++ src/main/java/proj/services/PostService.java
@@ -1,5 +1,8 @@
 package proj.services;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +18,15 @@ public class PostService{
 
   public Post findById(String id){
     return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+  }
+
+  public List<Post> findByTitle(String text){
+    //return repo.findByTitleContainingIgnoreCase(text);
+    return repo.searchTitle(text);
+  }
+
+  public List<Post> completeSearch(String text, Date minDate, Date maxDate){
+    maxDate = new Date(maxDate.getTime() + 86400000); // Add one day to the maxDate (milliseconds)
+    return repo.completeSearch(text, minDate, maxDate);
   }
 }
